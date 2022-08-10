959c53044af5f6d84c8282d4b110d65b5be49c6a
Ivan Doboni <ivan.doboni3006@gmail.com>
2022-08-10 20:33:04 +0300

    nmbnm

---
+++ script-1.js
@@ -0,0 +1,109 @@
+// const message = "Java ya tyt" 
+// console.log(message);
+
+
+// const userName = "Fruit";
+// console.log("userName", userName);
+
+        ////   Zavdania 111111;;;;modyl 1111
+
+//===============================
+// const productName = ("Droid");
+// const pricePerItem = (2000);
+// console.log(productName, pricePerItem);
+      
+
+      //Zavd 22222-----------mod 1 
+// let productName = "Droid";
+// let pricePerItem = 2000;
+
+// // Change code below this line
+
+// pricePerItem = 3500;
+// productName = 'Repair droid';
+///  я взяв продуктнейм за допомогою летом присвоїв нове значення
+
+             
+
+              ////zavd 333-----mod 1
+
+// Change code below this line
+// let topSpeed = 160;
+// let distance = 617.54;
+// let login = "mango935";
+// const isOnline = true;
+// const isAdmin = false;
+// console.log(isOnline, isAdmin);
+
+
+              ///////zavd 4444-----mod 1
+
+// const pricePerItem = 3500;
+// const orderedQuantity = 4;
+
+// // Change code below this line
+// const totalPrice = pricePerItem*orderedQuantity;
+
+// console.log(totalPrice);
+           
+
+            //////zavd 5555----- mod 1
+// 			const productName = "Droid";
+// const pricePerItem = 3500;
+
+// // Change code below this line
+// const message = "You picked Droid, price per item is 3500 credits"
+// console.log(message);
+
+             //// zavd 6666 ---- mod 1
+			 // Change code below this line
+// const orderedQuantity = 6;
+// const pricePerDroid = 800;
+// const deliveryFee = 50;
+// const totalPrice = pricePerDroid*orderedQuantity+deliveryFee;
+// const message = `You ordered droids worth ${totalPrice} credits. Delivery (${deliveryFee} credits) is included in total price.`;
+// console.log(message);
+
+
+
+             //// zavd 7777 --- mod 1
+			 // Change code below this line
+// function sayHi (){
+//     console.log("Hello, this is my first function!")
+// }
+// sayHi();
+
+
+         /////zavd 888888 ---- mod 1
+		 // Change code below this line
+// function add(a, b, c) {
+// 	console.log(`Addition result equals ${a+b+c}`);
+// 	// Change code above this line
+//   }
+  
+//   add(15, 27, 10);
+//   add(10, 20, 30);
+//   add(5, 10, 15);
+
+//////////////////////////////////////////////
+// Num1 = 5;
+// Num2 = 5.5;
+// Str1 = "hello"
+// Str1 = 'World'
+// Str1 = null;
+// Bool1 = true;
+// document.write ("<br>Num1 - " + typeof (Num1));
+// document.write ("<br>Num2 - " + typeof (Num2));
+// document.write ("<br>Str1 - " + typeof (Str1));
+// document.write ("<br>Str2 - " + typeof (Str2));
+// document.write ("<br>Str3 - " + typeof (Srt3));
+// document.write ("<br>Bool1 - " + typeof (Bool1));
+
+// console.log();
+/////////////////////////////////////////
+const numbers = [6, 7, 8];
+const newNumbers = [...nambers];
+function foo(...args) {
+ console.log(args);
+};
+foo(1, 2, 3);
